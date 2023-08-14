e09c19fb7c2b8322a4f1f280ebd31b72906c4e11
X-DXLVUR <140062001+X-DXLVUR@users.noreply.github.com>
2023-08-14 12:08:42 +0530

    🙄😂

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "24854614"))
+API_HASH = getenv("API_HASH", "bb4a375c908f0681ed4083f4fb3a9ced")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6029802782:AAHHUAVtZofsLn6Sj3JVYT5BUdqeJlQF-9E")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 6164559838))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/soul_x6")
